cb913eb42e04d10648f21ad581ccf04966490053
王喆 <654391654@qq.com>
2020-11-07 13:52:18 +0800

    2sh

--- run_exp_cock_4.sh
+++ run_exp_cock_4.sh
@@ -11,10 +11,10 @@ for n_clients in 20 40
 do
         ssh xcnc35 "cd $project_dir && git pull && java -jar loadData.jar"
         echo "Reloaded DB"
-        ssh xcnc35 "cd $project_dir && git pull && bash run_node.sh $xact_dir 1 $n_clients $n_nodes ${ips[0]}" &
-        ssh xcnc36 "cd $project_dir && git pull && bash run_node.sh $xact_dir 2 $n_clients $n_nodes ${ips[1]}" &
-        ssh xcnc37 "cd $project_dir && git pull && bash run_node.sh $xact_dir 3 $n_clients $n_nodes ${ips[2]}" &
-        ssh xcnc38 "cd $project_dir && git pull && bash run_node.sh $xact_dir 4 $n_clients $n_nodes ${ips[3]}" &
+        ssh xcnc35 "cd $project_dir && git pull && bash run_node_cock.sh $xact_dir 1 $n_clients $n_nodes ${ips[0]}" &
+        ssh xcnc36 "cd $project_dir && git pull && bash run_node_cock.sh $xact_dir 2 $n_clients $n_nodes ${ips[1]}" &
+        ssh xcnc37 "cd $project_dir && git pull && bash run_node_cock.sh $xact_dir 3 $n_clients $n_nodes ${ips[2]}" &
+        ssh xcnc38 "cd $project_dir && git pull && bash run_node_cock.sh $xact_dir 4 $n_clients $n_nodes ${ips[3]}" &
         wait
         echo "All nodes finished"
         dbstate_csv=${n_clients}_${n_nodes}_dbstate.csv
